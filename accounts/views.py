cd7b964a18b5ef35e08f38242f3d80d0876df4fd
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-23 21:52:31 +0530

    first views created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home_view(request):
+    return HttpResponse("<h1>Home</h1>")
+
+
+def about_view(request):
+    return HttpResponse("<h1>About</h1>")
