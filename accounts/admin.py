5e51ae880e4ea6114879d791dc36b81ff17b58f0
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-25 22:24:45 +0530

    admin update.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, Product, Order
+
+
+# class OrderInline(admin.TabularInline):
+#     model = Order
 
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'phone', 'create_date', 'write_date']
     list_display_links = ['name', 'email', 'phone', 'create_date', 'write_date']
-    
+    date_hierarchy = 'create_date'
+    # inlines = [
+    #     OrderInline,
+    #     ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category', 'create_date', 'write_date']
+    list_display_links = ['name', 'price', 'category', 'create_date', 'write_date']
+    date_hierarchy = 'create_date'
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['status', 'create_date', 'write_date']
+    list_display_links = ['status', 'create_date', 'write_date']
+    date_hierarchy = 'create_date'
