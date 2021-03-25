27c918f30176bd1dc381a1f0fac1d1a7ce9341d8
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-25 22:03:30 +0530

    cx model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 
+class Customer(models.Model):
+    name = models.CharField(max_length=250, null=True)
+    phone = models.CharField(max_length=250, null=True)
+    email = models.CharField(max_length=250, null=True)
+    create_date = models.DateTimeField(auto_now=True)
+    write_date = models.DateTimeField(auto_now_add=True)
